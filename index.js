b400ea5050809440d58302c9068a1a72165a8841
chastenr <chasten67@yahoo.com>
2025-07-26 15:07:59 +0800

    asgg

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("about");
+    const aboutSection = document.getElementById("aboutmain");
+
+    button.addEventListener("click", function () {
+
+        aboutSection.scrollIntoView({ behavior: "smooth", });
+
+    });
+  });
+
+  document.addEventListener("DOMContenetLoaded", function () {
+    const button = document.getElementById("contact")
+  })
